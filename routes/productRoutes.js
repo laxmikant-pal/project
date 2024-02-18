2a9613d52eaea523cb52bd6b6449bf639d494dfe
laxmikant-pal <laxmikant.pal34@gmail.com>
2024-02-18 18:22:39 +0530

    my task assingent

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controller/productController');
 const authMiddleware = require('../middlerware/authMiddleware');
-console.log(authMiddleware.verifyToken);
+
 router.post('/create', authMiddleware.verifyToken, productController.createProduct);
 router.get('/displayall', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
