95ac9e27f92270d5aa6bb57b10a05d059cae916b
laxmikant-pal <laxmikant.pal34@gmail.com>
2024-02-18 18:15:34 +0530

    my task assingent

--- controller/productController.js
+++ controller/productController.js
@@ -32,7 +32,40 @@ exports.getAllProducts = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+//this code can short product by name and price 
+// exports.getAllProducts = async (req, res) => {
+//   try {
+//     const page = parseInt(req.query.page) || 1;
+//     const limit = parseInt(req.query.limit) || 10;
+//     const sortBy = req.query.sortBy || 'name'; // Default sorting by name
+//     const sortOrder = req.query.sortOrder || 'asc'; // Default sorting order ascending
 
+//     const startIndex = (page - 1) * limit;
+
+//     const sortOptions = {};
+//     if (sortBy === 'name') {
+//       sortOptions['name'] = sortOrder === 'asc' ? 1 : -1;
+//     } else {
+//       sortOptions[sortBy] = sortOrder === 'asc' ? 1 : -1;
+//     }
+
+//     const products = await Product.find()
+//       .sort(sortOptions)
+//       .limit(limit)
+//       .skip(startIndex);
+
+//     const totalCount = await Product.countDocuments();
+
+//     res.json({
+//       totalCount,
+//       currentPage: page,
+//       totalPages: Math.ceil(totalCount / limit),
+//       data: products
+//     });
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// };
 exports.getProductById = async (req, res) => {
   try {
     const product = await Product.findById(req.params.id);
